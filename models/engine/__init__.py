2d8dd5ac88dd644e625e542ba966a8a9c6909f0a
elassari19 <elassari19@gmail.com>
2023-10-15 23:02:52 +0100

    update multi files

--- models/engine/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Script to initialize a FileStorage instance and reload stored data.
-"""
-from models.engine.file_storage import FileStorage
-
-"fileStorage operations"
-storage = FileStorage()
-storage.reload()
